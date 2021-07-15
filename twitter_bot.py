033f68e2c8d4509d9bc086b0d4355ea14c0ed3d6
t00thle55 <62895012+t00thle55@users.noreply.github.com>
2021-07-14 22:08:37 -0400

    added runtime and proc

--- twitter_bot.py
+++ twitter_bot.py
@@ -18,15 +18,17 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+api.create_friendship(1005593216966590464)
+
 responses = ["Silence, you Canadian oppressor", "How about you shut up about issues you know nothing about.", "Woooowww...coming from a white colonizer", "Your ethnicity is a sham", "Go back to Britain...Oh wait..."]
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        if status.user.id_str != '1407713509950083074':
+        if status.user.id_str != '1005593216966590464':
             return
         bobified_string = self.bobify(status.text)
         #response = random.choice(responses(0, -1)(i) for i in responses)
-        api.update_status(f"@PastaAndCannoli {bobified_string[0:35]}...{random.choice(responses)}", status.id)
+        api.update_status(f"@MapleAndTea {bobified_string[0:35]}...{random.choice(responses)}", status.id)
 
 
     def bobify(self, message):
@@ -38,7 +40,7 @@ class MyStreamListener(tweepy.StreamListener):
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 
-myStream.filter(follow=['1407713509950083074'], is_async=True)
+myStream.filter(follow=['1005593216966590464'], is_async=True)
 
 # username = "PastaAndCannoli"
 
